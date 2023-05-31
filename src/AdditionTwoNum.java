60427f0a75fcad4fdbd1ab240526ccbcc47df8c2
shankurabhiram <shankurabhiram66@gmail.com>
2023-05-31 14:57:04 +0530

    addition

---
+++ src/AdditionTwoNum.java
@@ -0,0 +1,12 @@
+
+public class AdditionTwoNum {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=30; 
+		int res =num1+num2;
+		System.out.println("Addition of two numbers   "+ res);
+
+	}
+
+}
